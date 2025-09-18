83ead58a1f216257891d4b9f8a6fca67220a762a
cde123456781 <bjc199@uclive.ac.nz>
2025-09-19 01:54:38 +1200

    Adds model for movies

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -3,20 +3,24 @@ using System.Text.Encodings.Web;
 
 namespace MvcMovie.Controllers
 {
-    public class HelloController : Controller
+    public class HelloWorldController : Controller
     {
         //
         // GET: /HelloWorld/
-        public string Index()
+        public IActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
-        //
         // GET: /HelloWorld/Welcome/
-        public string Welcome()
+        // Requires using System.Text.Encodings.Web;
+        public IActionResult Welcome(string name="John Smith", int numTimes=1)
         {
-            return "This is the Welcome action method...";
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+            
+           
         }
     }
 }
--- MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35b59501c4159dff71f504bf22446fccd83741b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+943d63fd715b307874ef775e611567916a2c39f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
